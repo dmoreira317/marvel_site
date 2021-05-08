88f08ba93fb6e4d9ebed9da7abb8f597c69e2aab
dmoreira317 <dmoreiraventas@gmail.com>
2021-05-07 22:18:16 -0300

    Main paths set and working, with a view to test requests from server

--- marvelpedia/marvelapp01/views.py
+++ marvelpedia/marvelapp01/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse # This takes http requests
+
 
 # Create your views here.
+def vista1(request):
+    return HttpResponse("Hola Buenos dias")
--- marvelpedia/marvelpedia/urls.py
+++ marvelpedia/marvelpedia/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include
+from marvelapp01 import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", views.vista1, name="main" )
 ]
