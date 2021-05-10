612dd1614d61fee162b6b41bb53cace8e6e519ea
dmoreira317 <dmoreiraventas@gmail.com>
2021-05-10 14:52:09 -0300

    Modified the requests module, added some context comments and def as function

--- marvelpedia/marvelapp01/API_requests_marvel.py
+++ marvelpedia/marvelapp01/API_requests_marvel.py
@@ -10,18 +10,28 @@ from dotenv import load_dotenv
 
 #This isn't linked to anything as of now
 
-load_dotenv()
-TIME_STAMP = time.time()
-API_PUBLIC = os.getenv('API_PUBLIC')
-API_PRIVATE = os.getenv('API_PRIVATE')
-md5_origin = str(TIME_STAMP)+API_PRIVATE+API_PUBLIC
-HASH_CODE = hashlib.md5(md5_origin.encode())
-HASH = HASH_CODE.hexdigest()
+# Request to Marvel's dev site
+def API_request(url):
+    # Loading keys
+    load_dotenv()
+    TIME_STAMP = time.time()
+    API_PUBLIC = os.getenv('API_PUBLIC')
+    API_PRIVATE = os.getenv('API_PRIVATE')
+    
+    #Generating requested hash as parameter
+    md5_origin = str(TIME_STAMP)+API_PRIVATE+API_PUBLIC
+    HASH_CODE = hashlib.md5(md5_origin.encode())
+    HASH = HASH_CODE.hexdigest()
 
-params = {
-    "apikey": API_PUBLIC,
-    "ts": TIME_STAMP,
-    "hash": HASH,
-}
-test_request = requests.get("http://gateway.marvel.com/v1/public/comics", params=params)
-print(test_request)
+    # defining params and making the request
+    params = {
+        "apikey": API_PUBLIC,
+        "ts": TIME_STAMP,
+        "hash": HASH,
+    }
+    test_request = requests.get(url, params=params)
+    return print(test_request.text)
+
+# test search
+# url = "http://gateway.marvel.com/v1/public/comics"
+# API_request(url)
