9d795c41f0182169d6d5114e3cd75e2119ba3f3b
dmoreira317 <dmoreiraventas@gmail.com>
2021-06-02 22:21:13 -0300

    secured django private key to make repo public later on

--- marvelpedia/marvelpedia/settings.py
+++ marvelpedia/marvelpedia/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dotenv
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,7 +27,7 @@ print(STATIC_DIR)
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-()mlvl!ut_$p^r)c#z-!-)4x6h0nr34d^a0x*nqd^pks#-z^9a'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
